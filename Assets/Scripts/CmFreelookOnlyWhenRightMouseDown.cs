05f3394b5c2ed434f921c0edf11c150de3518528
xcodesmith <xcodesmith@gmail.com>
2020-10-06 14:56:34 -0400

    Free look camera menu

---
+++ Assets/Scripts/CmFreelookOnlyWhenRightMouseDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CmFreelookOnlyWhenRightMouseDown : MonoBehaviour
+{
+    void Start(){
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+    public float GetAxisCustom(string axisName){
+        if(axisName == "Mouse X"){
+            if (Input.GetMouseButton(1)){
+                return Input.GetAxis("Mouse X");
+            } else{
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y"){
+            if (Input.GetMouseButton(1)){
+                return Input.GetAxis("Mouse Y");
+            } else{
+                return 0;
+            }
+        }
+        return Input.GetAxis(axisName);
+    }
+}
