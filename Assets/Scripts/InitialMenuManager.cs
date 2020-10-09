1aea9e6d5c696332795c197f549849718f994e31
xcodesmith <xcodesmith@gmail.com>
2020-10-09 14:43:57 -0400

    Working on menu scene

---
+++ Assets/Scripts/InitialMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialMenuManager : MonoBehaviour
+{
+    private MenuSoundButton _menuSoundButton;
+    
+    private void Start()
+    {
+        _menuSoundButton = gameObject.GetComponent<MenuSoundButton>();
+    }
+
+    public void PlayButton()
+    {
+        _menuSoundButton.PlayButtonClickSound();
+    }
+    
+    public void SettingsButton()
+    {
+        _menuSoundButton.PlayButtonClickSound();
+    }
+}
---
+++ Assets/Scripts/MenuSoundButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSoundButton : MonoBehaviour
+{
+    private AudioSource _buttonSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _buttonSound = GameObject.Find("ButtonSound").gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlayButtonClickSound()
+    {
+        _buttonSound.Play();
+    }
+}
